952ff6a89f1072a913efa7abdb2e0471373703d8
Oleh <omatsik@ukr.net>
2019-12-01 18:09:50 +0200

    Meta teg

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
   <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="title" content="Luxury restaurant">
-  <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.">
-  <meta name="keywords" content="Luxury restaurant, Today Special, Special Menu">
+  <meta name="title" content="MarketArt">
+  <meta name="description" content="Нова революційна платформа, що дозволить вам поділитись вашим шедевром з усім світом.">
+  <meta name="keywords" content="Art, marketplace, paintings, gallery">
   <meta property="og:type" content="website">
-  <meta property="og:site_name" content="Luxury restaurant">
-  <meta property="og:title" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.">
-  <meta property="og:url" content="#">
+  <meta property="og:site_name" content="MarketArt">
+  <meta property="og:title" content="Нова революційна платформа, що дозволить вам поділитись вашим шедевром з усім світом.">
+  <meta property="og:url" content="https://volodymyrlegionua.github.io/StartUp/">
   <meta property="og:locale" content="en_EN">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/animate.css">
@@ -21,7 +21,7 @@
     <link rel="stylesheet" type="text/css" href="css/slick.css">
     <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
     <link rel="icon" type="image/png" href="img/logo.png">
-  <title>Luxury restaurant</title>
+  <title>MarketArt</title>
 </head>
 <body>
 
