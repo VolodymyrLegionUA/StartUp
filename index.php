c9fd8dc0e18d18238c957a48fbd12b818fc6108d
Volodymyr <volodymyr.fihurniak@gmail.com>
2019-12-01 17:58:44 +0200

    Start

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="title" content="Luxury restaurant">
+  <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.">
+  <meta name="keywords" content="Luxury restaurant, Today Special, Special Menu">
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="Luxury restaurant">
+  <meta property="og:title" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.">
+  <meta property="og:url" content="#">
+  <meta property="og:locale" content="en_EN">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/adaptation.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link rel="icon" type="image/png" href="img/logo.png">
+  <title>Luxury restaurant</title>
+</head>
+<body>
+
+    <script type="text/javascript" type='text/jscript' src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
